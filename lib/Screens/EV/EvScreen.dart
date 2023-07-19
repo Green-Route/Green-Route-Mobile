2ec139df1360d73005a0b7d07af704be61118a8b
mohit <mohit41999@gmail.com>
2023-07-19 13:58:09 -0400

    EV Screens

---
+++ lib/Screens/EV/EvScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EvHomeScreen extends StatefulWidget {
+  const EvHomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<EvHomeScreen> createState() => _EvHomeScreenState();
+}
+
+class _EvHomeScreenState extends State<EvHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
